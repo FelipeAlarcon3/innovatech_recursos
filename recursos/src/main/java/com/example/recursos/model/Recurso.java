e937a505daee87c66ef6a80b3c01b66d84c0dd41
Felipe Alarcon <fe.alarconz@duocuc.cl>
2026-05-12 21:30:37 -0400

    agrege cors config

--- recursos/src/main/java/com/example/recursos/model/Recurso.java
+++ recursos/src/main/java/com/example/recursos/model/Recurso.java
@@ -1,13 +1,7 @@
 package com.example.recursos.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Data
