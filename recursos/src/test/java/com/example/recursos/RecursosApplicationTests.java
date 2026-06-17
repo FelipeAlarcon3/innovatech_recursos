6840ac892d75c76ed6d9dbddacc7c2483d4f1b0f
Felipe Alarcon <fe.alarconz@duocuc.cl>
2026-06-16 23:06:50 -0400

    prueba unitaria actualizada

--- recursos/src/test/java/com/example/recursos/RecursosApplicationTests.java
+++ recursos/src/test/java/com/example/recursos/RecursosApplicationTests.java
@@ -1,13 +1,47 @@
 package com.example.recursos;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.recursos.model.Recurso;
+import com.example.recursos.repository.RecursoRepository;
+import com.example.recursos.service.RecursoService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RecursosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private RecursoRepository recursoRepository;
 
+    @Autowired
+    private RecursoService recursoService;
+
+    @Test
+    void contextLoads() {
+
+    }
+
+    @Test
+    void testGuardarYBuscarRecurso_FlujoReal() {
+
+        Recurso recurso = new Recurso();
+        recurso.setNombre("Felipe Alarcon");
+        recurso.setRol("Desarrollador");
+        recurso.setDisponible(true);
+
+
+        Recurso recursoGuardado = recursoRepository.save(recurso);
+
+
+        var idGenerado = recursoGuardado.getId();
+
+
+        Recurso resultado = recursoService.buscarPorId(String.valueOf(idGenerado)).orElse(null);
+
+
+        assertNotNull(resultado);
+        assertEquals("Felipe Alarcon", resultado.getNombre());
+    }
 }
